c060d36f6dd114017880406ec36adb2d5b6df9e3
Bayajit Mohammed <bayajitmohammed@gmail.com>
2025-08-14 09:49:23 +0600

    refactor: Clean up TodoList component template and improve code readability

--- src/components/TodoApp/TodoList.vue
+++ src/components/TodoApp/TodoList.vue
@@ -1,30 +1,24 @@
 <template>
   <div
-    class="bg-[#CAD7C5] m-5 mt-8 p-6 pb-8 pt-8 rounded-3xl shadow-[3px_3px_3px_0_#989e8e] relative h-72 flex flex-col"
+    class="bg-[#CAD7C5] m-5 mt-8 p-6 pb-8 pt-8 rounded-3xl shadow-[3px_3px_3px_0_#989e8e] relative h-72 flex flex-1 flex-col before:absolute before:-top-2 before:left-1/2 before:-translate-x-1/2 before:w-11/12 before:h-7 before:bg-size-[21px_28px] before:bg-[radial-gradient(circle_at_5%_45%,transparent_70%,#828E80_20%)]"
   >
-    <!-- Spiral binding decoration -->
-    <div class="spiral-pin absolute -top-[11px] left-0 right-0 flex gap-1 px-6">
-      <span
-        v-for="i in 9"
-        :key="i"
-        class="block rounded-full flex-1 aspect-square border-[3px] border-[#828E80]"
-      ></span>
-    </div>
-    
     <!-- Error Toast Notification -->
-    <div 
-      v-if="errorMessage" 
+    <div
+      v-if="errorMessage"
       class="absolute -top-2 left-1/2 transform -translate-x-1/2 z-50 bg-red-500 text-white px-3 py-2 rounded-lg shadow-lg max-w-xs"
     >
       <div class="flex items-center gap-2">
         <ion-icon :icon="alertCircle" class="text-sm flex-shrink-0"></ion-icon>
         <span class="text-xs">{{ errorMessage }}</span>
-        <button @click="clearError" class="ml-1 text-white hover:text-gray-200 flex-shrink-0">
+        <button
+          @click="clearError"
+          class="ml-1 text-white hover:text-gray-200 flex-shrink-0"
+        >
           <ion-icon :icon="close" class="text-sm"></ion-icon>
         </button>
       </div>
     </div>
-    
+
     <!-- Add new todo form -->
     <form
       @submit.prevent="addTodoItem()"
@@ -38,13 +32,19 @@
         :disabled="isLoading"
       />
       <button class="w-6 aspect-square" type="submit" :disabled="isLoading">
-        <div v-if="isLoading" class="animate-spin rounded-full h-5 w-5 border-2 border-[#828E80] border-t-transparent"></div>
+        <div
+          v-if="isLoading"
+          class="animate-spin rounded-full h-5 w-5 border-2 border-[#828E80] border-t-transparent"
+        ></div>
         <ion-icon v-else class="text-[#828E80] text-2xl" :icon="add"></ion-icon>
       </button>
     </form>
-    
+
     <!-- Todo items list -->
-    <div class="flex flex-col flex-1 no-scrollbar overflow-y-scroll" v-auto-animate>
+    <div
+      class="flex flex-col flex-1 no-scrollbar overflow-y-scroll"
+      v-auto-animate
+    >
       <TodoItem
         v-for="item in sortedTodoItems"
         :key="item.id"
